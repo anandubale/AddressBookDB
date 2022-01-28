6aa941a6d1479aef552678750b593592bb3b3f46
anandubale <au26199@gmail.com>
2022-01-28 16:39:54 +0530

    Inserted Contact in AddressBook Table

---
+++ UC3.sql
@@ -0,0 +1,16 @@
+CREATE TABLE AddressBook
+(
+firstName varchar(20) not null,
+lastName varChar(20) not null,
+address varchar(100) not null,
+city varchar(20) not null,
+state varchar(20) not null,
+zip int not null,
+phoneNumber varchar(12),
+email varchar(max) not null
+)
+select*
+from AddressBook
+
+insert into AddressBook (firstName,lastName,address,city,state,zip,phoneNumber,email) values('Anand','ubale','Pawansutnagar','Mehkar','Maharastra','443301','7888289884','au11@gmail.com')
+insert into AddressBook values('mayur','kalekar','Pawansutnagar','Mehkar','Maharastra','443301','1234567890','mkalekar25@gmail.com');
