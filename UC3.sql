c5bd9ca0f6e4f10210ad0029ee7099f756668440
anandubale <au26199@gmail.com>
2022-01-28 16:47:40 +0530

    Edit Contact using update-set-where

--- UC3.sql
+++ UC3.sql
@@ -14,3 +14,7 @@ from AddressBook
 
 insert into AddressBook (firstName,lastName,address,city,state,zip,phoneNumber,email) values('Anand','ubale','Pawansutnagar','Mehkar','Maharastra','443301','7888289884','au11@gmail.com')
 insert into AddressBook values('mayur','kalekar','Pawansutnagar','Mehkar','Maharastra','443301','1234567890','mkalekar25@gmail.com');
+
+
+update AddressBook set city='buldana' where firstName = 'mayur'
+update AddressBook set address='ekataNagar' where firstName = 'mayur'
